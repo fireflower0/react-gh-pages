b9ec99b085f76de6ab34381efa63ae0305086510
fireflower0 <fireflower0@hotmail.co.jp>
2021-03-14 09:01:28 +0900

    Add a base style

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar';
@@ -6,6 +6,12 @@ import Home from './Home';
 import About from './About';
 
 const App = () => {
+  useEffect(() => {
+    document.title = 'My Home Page';
+    document.body.style.margin = '0';
+    document.body.style.backgroundColor = '#9E9E9E';
+  }, []);
+
   return (
     <div>
       <Router>
