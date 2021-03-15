13150eccf16fb5b3372089f296ed6511a0af9c7d
fireflower0 <fireflower0@hotmail.co.jp>
2021-03-16 08:23:57 +0900

    Fixed a style

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
+
+const FormWrapper = styled.div`
+  margin: 10px;
+  padding: 10px;
+  background-color: #E0F2F1;
+`;
 
 const App = () => {
   useEffect(() => {
     document.title = 'My Home Page';
     document.body.style.margin = '0';
-    document.body.style.backgroundColor = '#9E9E9E';
+    document.body.style.backgroundColor = '#ECEFF1';
   }, []);
 
   return (
-    <div>
+    <FormWrapper>
       <Router>
         <Navbar />
         <hr />
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
       </Router>
-    </div>
+    </FormWrapper>
   );
 };
 
